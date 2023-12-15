eabdde98eacb6d1e676676a1d1a83adb0f970d02
BirdiD <diioula.doucoure@gmail.com>
2023-12-15 12:20:06 +0100

    ADD TRANSCRIPTS SAVING

--- src/pages/utils.py
+++ src/pages/utils.py
@@ -39,16 +39,31 @@ def generate_unique_filename():
     Generate a unique filename with a timestamp.
     """
     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-    return f"{str(uuid.uuid4())}_{timestamp}.wav"
+    return f"{str(uuid.uuid4())}_{timestamp}.wav", f"{str(uuid.uuid4())}_{timestamp}.txt"
 
 
 
-def upload_to_drive(audio, drive_folder_id="1bkxELyDOA98Ok5uZP3Q0yBoMFE4jy0q5"):
+def upload_to_drive(audio, text="Neddo ko banndum", audio_folder_id="1bkxELyDOA98Ok5uZP3Q0yBoMFE4jy0q5", text_folder_id = "1r0ksPIs8ooe2mpm3-WvK9iQBTaybYojg"):
     """
-    Upload the audio bytes to Google Drive.
+    Upload the audio bytes  and text to Google Drive.
+    Params:
+        audio : Recorded audio from browser
+        text : Model transcription
+        audio_folder_id : Folder where audio are stored
+        text_folder_id : Folder where transcripts are stored
     """
-    filename = generate_unique_filename()
     service = authenticate()
-    file_metadata = {'name': filename, 'parents': [drive_folder_id]}
-    media = MediaIoBaseUpload(io.BytesIO(audio), mimetype='audio/wav')
-    service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    audio_filename, txt_filename = generate_unique_filename()
+    
+
+    audio_file_metadata = {'name': audio_filename, 'parents': [audio_folder_id]}
+    audio_media = MediaIoBaseUpload(io.BytesIO(audio), mimetype='audio/wav')
+
+    txt_file_metadata = {'name': txt_filename, 'parents': [text_folder_id]}
+    txt_media = MediaIoBaseUpload(io.BytesIO(text.encode('utf-8')), mimetype='text/plain')
+
+    service.files().create(body=audio_file_metadata, media_body=audio_media, fields='id').execute()
+    service.files().create(body=txt_file_metadata, media_body=txt_media, fields='id').execute().get('id')
+
+
+
